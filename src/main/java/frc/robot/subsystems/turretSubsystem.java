6f1b4c0a4ade7a9f84b9a300a3bcf3d36b6979ac
Logan Foster <44828794+Lrfoster03@users.noreply.github.com>
2021-02-23 23:26:50 -0800

    Began working on turret code. Not finished

--- src/main/java/frc/robot/subsystems/turretSubsystem.java
+++ src/main/java/frc/robot/subsystems/turretSubsystem.java
@@ -10,6 +10,11 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -27,17 +32,18 @@ import static frc.robot.Constants.digitalIOConstants.dio7_turretLimit;
 public class turretSubsystem extends SubsystemBase {
 
   //Neo 550 is the Motor Controlling the Turret
-  //private CANSparkMax turretDrive = new CANSparkMax(turret, MotorType.kBrushless);
-  private TalonSRX turretDrive = new TalonSRX(turret);
+  private CANSparkMax turretDrive = new CANSparkMax(turret, MotorType.kBrushless);
+  //private TalonSRX turretDrive = new TalonSRX(turret);
+  private CANEncoder m_encoder;
+  private CANPIDController m_pidController;
   private DigitalInput limit = new DigitalInput(dio7_turretLimit);
 
   public turretSubsystem() {
-    //turretDrive.restoreFactoryDefaults();
-    turretDrive.configFactoryDefault();
-    turretDrive.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, kIndex, kTimeout);
+    turretDrive.restoreFactoryDefaults();
+    m_encoder = turretDrive.getEncoder();
 
-    turretDrive.setInverted(false);
-    turretDrive.setSensorPhase(false);
+    //Sets Turret Position as 0
+    m_encoder.setPosition(0);
 
     turretDrive.configForwardSoftLimitThreshold(12308, kTimeout);
     turretDrive.configReverseSoftLimitThreshold(-11629, kTimeout);
@@ -50,12 +56,17 @@ public class turretSubsystem extends SubsystemBase {
     // zero the position. start position becomes center
     turretDrive.setSelectedSensorPosition(0, kIndex, kTimeout);
 
-    // TODO: tune PIDF parameters (these are only a guess)
-    turretDrive.configAllowableClosedloopError(0, kIndex, kTimeout);
-    turretDrive.config_kF(kIndex, 0.38, kTimeout);
-    turretDrive.config_kP(kIndex, 0.1, kTimeout);
-    turretDrive.config_kI(kIndex, 0, kTimeout);
-    turretDrive.config_kD(kIndex, 0, kTimeout);
+
+     // Set PID coefficients. Currently default
+     m_pidController.setP(0.2);
+     m_pidController.setI(1e-4);
+     m_pidController.setD(0);
+     m_pidController.setFF(0);
+     m_pidController.setIZone(100);
+     m_pidController.setOutputRange(-1, 1);
+
+    
+     turretDrive.configAllowableClosedloopError(0, kIndex, kTimeout);
 
     // set Motion Magic max Cruise Velocity and max acceleration
     turretDrive.configMotionCruiseVelocity((int) (kMaxDegreesPerSecond / (kDegreesPerTick * 10)),
@@ -69,7 +80,7 @@ public class turretSubsystem extends SubsystemBase {
    * turretHome()  rotate turret to zero degrees
    */
   public void turretHome() {
-    turretDrive.set(ControlMode.Position, 0);
+     m_encoder.setPosition(0);
   }
 
   /**
@@ -85,6 +96,7 @@ public class turretSubsystem extends SubsystemBase {
       angleDeg = kSoftMaxTurretAngle;
     }
 
+    m_encoder.setPosition();
     turretDrive.set(ControlMode.Position, angleDeg / kDegreesPerTick);
   }
 
@@ -104,8 +116,13 @@ public class turretSubsystem extends SubsystemBase {
     setPercentOutput(0);
   }
 
+   /**
+   * setPercentOutput()  - override Turret Motor with percent output
+   * 
+   * @param percent, percent motor output -1.0 to 1.0
+   */
   public void setPercentOutput(double percent) {
-    turretDrive.set(ControlMode.PercentOutput, percent);
+    turretDrive.set(percent);
   }
 
   /**
@@ -114,7 +131,7 @@ public class turretSubsystem extends SubsystemBase {
    * @return angle in degrees
    */
   public double getAngleDegrees() {
-    return (turretDrive.getSelectedSensorPosition() * kDegreesPerTick);
+    return (m_encoder.getPosition() * kDegreesPerTick);
   }
 
   /**
@@ -130,7 +147,7 @@ public class turretSubsystem extends SubsystemBase {
   public void periodic() {
     boolean turretLimit = !limit.get();
 
-    double pos = turretDrive.getSelectedSensorPosition();
+    double pos = m_encoder.getPosition();
 
     SmartDashboard.putBoolean("TurretLimit", turretLimit);
     SmartDashboard.putNumber("Turret Pos", pos);
