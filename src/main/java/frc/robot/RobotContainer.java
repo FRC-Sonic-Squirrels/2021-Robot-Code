ba5bd83ba55c7c129c5937a430b1193edc076e57
Lrfoster03 <lrfoster03@gmail.com>
2021-03-25 18:55:37 -0700

    Changed X-value for Galactic Search

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -542,7 +542,7 @@ public class RobotContainer {
 
       if(m_limelightPowerCell.getTY()  < 30){
         //If PowerCell is less than 30 degrees off center, then we pick Red A path
-        if(m_limelightPowerCell.getTX()  < 30){
+        if(m_limelightPowerCell.getTX()  < 0){
           selectedPath = "RedA";
         }
         else {
@@ -551,7 +551,7 @@ public class RobotContainer {
       }
       else {
         //If PowerCell is less than 30 degrees off center, then we pick Blue A path
-        if(m_limelightPowerCell.getTX()  < 30){
+        if(m_limelightPowerCell.getTX()  < 0){
           selectedPath = "BlueA";
         }
         else {
